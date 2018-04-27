95b3e45825744dc7c58aca079e67671ad14927df
technoforrest <31766195+technoforrest@users.noreply.github.com>
2018-04-26 22:19:11 -0700

    to dos in unable to toggle, title undefined

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-$('#buttonClick').click (() => {
+'use strict'
+$('document').ready(function() {
     $.ajax({
         url:"https://jsonplaceholder.typicode.com/users",
-        type: "USER",
+        type: "GET",
         datatype: "json",
     })
     .done(function(response){
@@ -19,18 +20,114 @@ $('#buttonClick').click (() => {
 
     function displayUsers(data){
         for(let user of data){
-            console.log(user);
             let userContainer = document.createElement('div');
             userContainer.id = user.id;
-            let userTitle = document.createElement('h3');
-            userTitle.className = 'user-title';
-            let userBody = document.createElement('p');
-            userBody.className= 'user-body';
-            userBody.innerHTML= user.body;
+            userContainer.className = 'user-container';
+            
+            let userName = document.createElement('h4');
+            userName.className = 'user-name';
+            userName.innerHTML= user.name;
 
-            $('.user').append(userContainer);
-            $(`#${user.id}`).append(userTitle);
-            $(`#${user.id}`).append(userBody);
+            let userEmail = document.createElement('p');
+            userEmail.className= 'user-email';
+            userEmail.innerHTML= user.email;
+
+            let userCompany = document.createElement('p');
+            userCompany.className= 'user-company';
+            userCompany.innerHTML= user.company.name;
+
+            let userToDo = document.createElement('button');
+           userToDo.id = `user-todo-${user.id}`;
+           userToDo.innerHTML = 'TO DO';
+
+        //    let userAlbum = document.createElement('button');
+        //    userAlbum.id = `user-albums-${user.id}`;
+        //    userToDo.innerHTML = 'ALBUM';
+
+            $('.users').append(userContainer);
+            $(`#${user.id}`).append(userName);
+            $(`#${user.id}`).append(userEmail);
+            $(`#${user.id}`).append(userCompany);
+            $(`#${user.id}`).append(userToDo);
+            //$(`#${user.id}`).append(userAlbum);
+
+            $(`#user-todo-${user.id}`).click((event) => {
+               
+                toDoClick(event, user.id);
+    
+            })
+            // $(`#user-albums-${user.id}`).click((event) => {
+            //     albumClick(event, user.id);
+            // })
+        
         }
     }
+    
 });
+function toDoClick(){
+    if(event.target.dataset.loaded)
+    {
+        $('.todo-container').toggle();
+    }
+    else {
+        $.ajax({
+            url:"https://jsonplaceholder.typicode.com/todos",
+            type: "GET",
+            datatype: "json",
+        })
+        .done(function(response){
+            displayToDos(response);
+        })
+        .fail(function(status, errorThrown){
+            console.log("Error", errorThrown);
+            console.log("Status", status);
+        })
+        .always(function(status){
+            console.log("The request is complete.");
+        });
+    }
+}
+function displayToDos(data){
+       
+    for(let todo of data){
+        let toDoId = document.createElement('div');
+        toDoId.id = todo.id;
+        toDoId.className = 'todo-container';
+
+        let title = document.createElement('p');
+        title.className= 'todo-title';
+        title.innerHTML= toDoClick.title;
+
+        let completed = document.createElement('p');
+        completed.className= 'todo-completed';
+        completed.innerHTML= todo.completed;
+
+        $('.todo').append(toDoId);
+        $(`#${todo.id}`).append(title);
+        $(`#${todo.id}`).append(completed);
+    }
+}
+
+function albumClick(){
+    if(event.target.dataset.loaded)
+    {
+        $('.album-container').toggle();
+    }
+    else {
+        $.ajax({
+            url:"https://jsonplaceholder.typicode.com/albums",
+            type: "GET",
+            datatype: "json",
+        })
+        .done(function(response){
+            displayUsers(response);
+        })
+        .fail(function(status, errorThrown){
+            console.log("Error", errorThrown);
+            console.log("Status", status);
+        })
+        .always(function(status){
+            console.log("The request is complete.");
+        });
+    }
+}
